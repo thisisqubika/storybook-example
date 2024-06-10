ef661a19f406c4eadf44e03edf5f46d931124f38
Leonardo Val <leonardo.val@qubika.com>
2024-06-09 23:53:03 -0300

    Add stories with mocked data

--- src/components/BooksScreen/BooksScreen.stories.tsx
+++ src/components/BooksScreen/BooksScreen.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { http, HttpResponse } from 'msw';
 import { ThemeWrapper } from '../../common/themes';
 import { QueryClientWrapper } from '../../common/query';
+import { fakeSearch } from '../../../test/fixtures/openLibrary';
+import { baseURL } from '../../services/openLibrary';
 import { BooksScreen } from '.';
 
 export default {
@@ -23,3 +26,41 @@ export const Base: StoryObj = {
     );
   },
 }; // const Base
+
+export const MockedAPI_Success: StoryObj = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get(`${baseURL}search.json`, ({ request }) => {
+          const requestURL = new URL(request.url);
+          const q = requestURL.searchParams.get('q');
+          return HttpResponse.json(fakeSearch(q ?? ''));
+        }),
+      ],
+    },
+  }, // parameters
+
+  render() {
+    return (
+      <BooksScreen />
+    );
+  },
+}; // const MockedAPI_Success
+
+export const MockedAPI_Error: StoryObj = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get(`${baseURL}search.json`, () => {
+          return HttpResponse.json({}, { status: 404 });
+        }),
+      ],
+    },
+  }, // parameters
+
+  render() {
+    return (
+      <BooksScreen />
+    );
+  },
+}; // const MockedAPI_Error
