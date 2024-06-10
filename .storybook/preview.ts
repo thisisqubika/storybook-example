8a2a734036cf4483be4379ea7f1c26a618ea4d82
Leonardo Val <leonardo.val@qubika.com>
2024-06-09 23:20:05 -0300

    Install MSW & msw-storybook-addon

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,15 @@
 import type { Preview } from "@storybook/react";
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+// See <https://github.com/mswjs/msw-storybook-addon#configuring-msw>.
+initialize({
+  onUnhandledRequest: 'bypass',
+});
 
 const preview: Preview = {
+  loaders: [
+    mswLoader
+  ],
   parameters: {
     controls: {
       matchers: {
